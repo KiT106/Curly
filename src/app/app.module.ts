deb681d76d783a2de87e474abb619eab763b8895
Đặng Minh Dũng <dungdm93@live.com>
2016-11-01 16:56:16 +0700

    The Hero Editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule}   from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import {AppComponent}  from './app.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

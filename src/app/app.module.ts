0ab91ddc6fc8fd2a77bf3f65e15cf0cd1d557255
Đặng Minh Dũng <dungdm93@live.com>
2016-11-03 16:31:12 +0700

    Search-by-name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { RouterModule }   from '@angular/router';
 
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
+import { routes }               from './app.routes';
 
 import { AppComponent }        from './app.component';
 import { HeroComponent }       from './hero.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
 import { DashboardComponent }  from './dashboard.component';
-import { routes }    from './app.routes';
+import { HeroSearchComponent } from './hero-search.component';
 
 @NgModule({
   imports: [
@@ -26,7 +27,8 @@ import { routes }    from './app.routes';
     AppComponent,
     DashboardComponent,
     HeroComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
